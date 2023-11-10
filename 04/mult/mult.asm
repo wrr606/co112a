cf40d815613155e7a89626fbbd7b48e032128e35
wrr606 <wrr606@gmail.com>
2023-11-10 10:53:25 +0800

    er

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@2
+M=0
+
+(loop)
+@1
+D=M
+@exit
+D;JLE
+
+@0
+D=M
+@2
+M=M+D
+
+@1
+M=M-1
+
+@loop
+0;JMP
+
+(exit)
+@exit
+0;JMP
